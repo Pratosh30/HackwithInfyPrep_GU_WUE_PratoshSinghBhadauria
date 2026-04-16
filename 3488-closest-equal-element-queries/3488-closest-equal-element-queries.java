3e9c5e821fa0b7482ed5c7faa784b105e3bd28bb
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-04-16 10:17:48 +0530

    Time: 54 ms (88.89%), Space: 173.9 MB (95.96%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.java
@@ -1,40 +1,48 @@
 class Solution {
+    private int circularDistance(int from, int to, int n) {
+        if(from <= to)
+            return to - from;
+        return n - from + to;
+    }
     public List<Integer> solveQueries(int[] nums, int[] queries) {
-        int n = nums.length;
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        /**
+         0 1 2 3 4 5 6
+        [1,3,1,4,1,3,2]
+                 i
+        {
+            1: 4,
+            3: 1,
+            4: 3,
 
-        // store indices
-        for (int i = 0; i < n; i++) {
-            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            
         }
+        [dist(0, 2)=2, -1, dist(2, 0)=6-2+1 = 4]
 
-        List<Integer> ans = new ArrayList<>();
-
-        for (int q : queries) {
-            List<Integer> v = map.get(nums[q]);
-
-            // only one time present
-            if (v.size() == 1) {
-                ans.add(-1);
-                continue;
+        dist(a,b): a<b => b-a
+                   else => n-a+b+1)
+         */
+         int[] distances = new int[nums.length];
+         Map<Integer, int[]> lastSeen = new HashMap<>();
+         for(int i=0; i<nums.length; i++) {
+            distances[i] = Integer.MAX_VALUE;
+            if(lastSeen.containsKey(nums[i])) {
+                // calculate distance on the old item
+                int lastSeenIdxLeft = lastSeen.get(nums[i])[0];
+                int lastSeenIdxRight = lastSeen.get(nums[i])[1];
+                distances[lastSeenIdxRight] = Math.min(distances[lastSeenIdxRight], circularDistance(lastSeenIdxRight, i, nums.length));
+                distances[lastSeenIdxLeft] = Math.min(distances[lastSeenIdxLeft], circularDistance(i, lastSeenIdxLeft, nums.length));
+                distances[i] = Math.min(circularDistance(lastSeenIdxRight, i, nums.length),
+                circularDistance(i, lastSeenIdxLeft, nums.length));
+                lastSeen.put(nums[i], new int[] {lastSeen.get(nums[i])[0],i});
+            } else {
+                lastSeen.put(nums[i], new int[] {i,i});
             }
-
-            int pos = Collections.binarySearch(v, q);
-            int res = Integer.MAX_VALUE;
-
-            // left neighbor
-            int left = v.get((pos - 1 + v.size()) % v.size());
-            int d1 = Math.abs(q - left);
-            res = Math.min(res, Math.min(d1, n - d1));
-
-            // right neighbor
-            int right = v.get((pos + 1) % v.size());
-            int d2 = Math.abs(q - right);
-            res = Math.min(res, Math.min(d2, n - d2));
-
-            ans.add(res);
+         }
+        
+        List<Integer> result = new ArrayList<>();
+        for(int queryIdx : queries) {
+            result.add(distances[queryIdx] == Integer.MAX_VALUE ? -1 : distances[queryIdx]);
         }
-
-        return ans;
+        return result;
     }
 }
