28044a9521be708c2d43059c6248966e31b85351
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-04-26 08:19:31 +0530

    Time: 7 ms (100%), Space: 124.2 MB (91.65%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,42 +1,55 @@
 class Solution {
-    int[] dx = {1,-1,0,0};
-    int[] dy = {0,0,1,-1};
-
     public boolean containsCycle(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        int rows = grid.length;
+        int cols = grid[0].length;
 
-        boolean[][] vis = new boolean[n][m];
+        // every node is its own group
+        int[] groups = new int[rows * cols];
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(!vis[i][j]){
-                    if(dfs(i, j, -1, -1, grid, vis)) return true;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                groups[i * cols + j] = i * cols + j;
+            }
+        }
+
+        // we merge n1 and n2 if they are neighbours (adjacent + same value)
+        // if they already in the same group, then we found a cycle!
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                int id = i * cols + j;
+                char c = grid[i][j];
+
+                // find all neighbours, get all neighbour groups
+                // if already. merged --> cycle!
+
+
+                if (i + 1 < rows && grid[i+1][j] == c) {
+                    int aGroup = find(id, groups);
+                    int bGroup = find( (i + 1) * cols + j, groups);
+
+                    if (aGroup == bGroup) return true;
+                    groups[aGroup] = bGroup;
+                }
+
+                if (j + 1 < cols && grid[i][j+1] == c) {
+                    int aGroup = find(id, groups);
+                    int bGroup = find( i * cols + j + 1, groups);
+
+                    if (aGroup == bGroup) return true;
+                    groups[aGroup] = bGroup;
                 }
             }
         }
+
         return false;
     }
 
-    public boolean dfs(int x, int y, int px, int py, char[][] grid, boolean[][] vis){
-        vis[x][y] = true;
-
-        int n = grid.length;
-        int m = grid[0].length;
-
-        for(int d = 0; d < 4; d++){
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-            if(grid[nx][ny] != grid[x][y]) continue;
-
-            if(vis[nx][ny] && !(nx == px && ny == py)) return true;
-
-            if(!vis[nx][ny]){
-                if(dfs(nx, ny, x, y, grid, vis)) return true;
-            }
-        }
-        return false;
+    private int find(int id, int[] groups) {
+        if (id == groups[id]) return id;
+        groups[id] = find(groups[id], groups);
+        return groups[id];
     }
 }
+
