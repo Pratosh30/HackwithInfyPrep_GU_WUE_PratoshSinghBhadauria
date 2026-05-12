74bcfba478f0323f0f0be75ab83bac1b52a7d9d4
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-05-12 11:41:56 +0530

    Time: 23 ms (88.46%), Space: 114 MB (40.38%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -1,23 +1,19 @@
 class Solution {
-    public int minimumEffort(int[][] shop) {
-        Arrays.sort(shop, (a, b) -> b[1] - b[0] - (a[1] - a[0]));
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
 
-        int start = shop[0][1];
-        int bal = shop[0][1] - shop[0][0];
-        int loan = 0;
+        int energy = 0;
+        int curr = 0;
 
-        for (int i = 1; i < shop.length; i++) {
-            int cost = shop[i][0];
-            int thresh = shop[i][1];
-
-            if (bal < thresh) {
-                loan += thresh - bal;
-                bal = thresh;
+        for (int[] task : tasks) {
+            if (curr < task[1]) {
+                energy += task[1] - curr;
+                curr = task[1];
             }
 
-            bal -= cost;
+            curr -= task[0];
         }
 
-        return start + loan;
+        return energy;
     }
 }
