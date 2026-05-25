602e509ee00dab7922131612e72a3a00bc20cea1
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-05-25 10:00:38 +0530

    Time: 5 ms (100%), Space: 47.7 MB (81.35%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.java
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -1,26 +1,29 @@
 class Solution {
-    public boolean canReach(String s, int minJ, int maxJ) {
-        int n = s.length();
-
-        if (s.charAt(n - 1) == '1')
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int start = 0, end = 0, len = s.length();
+        if(len == 0 || s.charAt(0) == '1' || s.charAt(len-1) == '1') {
             return false;
+        }
+        boolean[] dp = new boolean[len];
+        dp[0] = true;
 
-        int[] dp = new int[n];
-        dp[0] = 1;
-        int reach = 0, maxR = maxJ;
+        for(int i = 0; i < len; i++) {
+            if(!dp[i]) {
+                continue;
+            }
 
-        for (int i = minJ; i < n; i++) {
-            if (i > maxR) return false;
+            start = Math.max(end + 1, i + minJump);
+            end = Math.min(len-1, i + maxJump);
 
-            reach += dp[i - minJ];
-            if (i > maxJ) reach -= dp[i - maxJ - 1];
-
-            if (reach > 0 && s.charAt(i) == '0') {
-                dp[i] = 1;
-                maxR = i + maxJ;
+            for(int j = start; j <= end; j++) {
+                if(s.charAt(j) == '0') {
+                    dp[j] = true;
+                }
+            }
+            if(dp[len-1]) {
+                return true;
             }
         }
-
-        return reach > 0;
+        return dp[len-1];
     }
 }
