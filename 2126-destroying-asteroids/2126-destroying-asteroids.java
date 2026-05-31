35881970308dbe194044187b934f2a16fbb221a7
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-05-31 16:41:51 +0530

    Time: 5 ms (99.54%), Space: 106 MB (92.61%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,17 +1,23 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-        Arrays.sort(asteroids);
-
-        long currentMass = mass;
-
-        for (int asteroid : asteroids) {
-            if (currentMass < asteroid) {
-                return false;
-            }
-
-            currentMass += asteroid;
+        int min = 100000;
+        int max = 0;
+        for(int x : asteroids) {
+            max = Math.max(x, max);
+            min = Math.min(x, min);
         }
 
+        int[] freq = new int[max + 1];
+        long val = mass;
+        for(int x : asteroids) {
+            if(x > val) freq[x]++;
+            else val += x;
+        }
+        if(val >= max) return true;
+        for(int i = min; i <= max; i++) {
+            if(val < i) return false;
+            if(freq[i] != 0) val += i * freq[i];
+        }
         return true;
     }
 }
