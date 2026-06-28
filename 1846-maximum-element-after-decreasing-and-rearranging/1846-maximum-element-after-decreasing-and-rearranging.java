33fcc218807193783227dd4a23a09a5bfc66b39f
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-06-28 22:21:20 +0530

    Time: 2 ms (100%), Space: 77.3 MB (78.75%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,15 +1,23 @@
 class Solution {
+    private static final int MAXN = 100000;
+    private static int[] freqs = new int[MAXN];
+
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-
-        Arrays.sort(arr);
-
-        arr[0] = 1;
-
-        for (int i = 1; i < arr.length; i++) {
-
-            arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+        final int n = arr.length;
+        if (n == 1) return 1;
+        for (int v : arr) {
+            if (v <= n) freqs[v - 1]++;
         }
-
-        return arr[arr.length - 1];
+        int maxe = n, c = -1;
+        for (int v = 0; v < maxe; v++) {
+            c += freqs[v];
+            if (c > v) {
+                maxe -= c - v;
+                c = v;
+            }
+            freqs[v] = 0;
+        }
+        Arrays.fill(freqs, maxe, n, 0);
+        return maxe;
     }
 }
