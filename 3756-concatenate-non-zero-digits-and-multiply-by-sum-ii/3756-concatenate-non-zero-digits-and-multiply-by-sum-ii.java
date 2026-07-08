9620f56832edbb07c4507d1427b46744d25d8455
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-07-08 13:44:29 +0530

    Time: 16 ms (93.33%), Space: 128.8 MB (65%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -1,50 +1,24 @@
 class Solution {
     public int[] sumAndMultiply(String s, int[][] queries) {
-        int n = s.length();
-        int m = queries.length;
-        long MOD = 1000000007L;
-
-        long[] prefSum = new long[n + 1];
-        long[] prefVal = new long[n + 1];
-        int[] prefCnt = new int[n + 1];
-        long[] power = new long[n + 1];
-
-        power[0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            power[i] = (power[i - 1] * 10) % MOD;
-        }
-
+        int m = queries.length, n = s.length();
+        int[] counts = new int[n+1];
+        long[] sums = new long[n+1], pows = new long[n+1], pow10 = new long[n+1];
+        long mod = (long) 1e9 + 7L, x = 0L, y = 1L;
+        pow10[0] = 1;
         for (int i = 0; i < n; i++) {
-            int d = s.charAt(i) - '0';
-
-            prefSum[i + 1] = prefSum[i] + d;
-            prefCnt[i + 1] = prefCnt[i] + (d != 0 ? 1 : 0);
-
-            if (d == 0) {
-                prefVal[i + 1] = prefVal[i];
-            } else {
-                prefVal[i + 1] = (prefVal[i] * 10 + d) % MOD;
-            }
+            var d = s.charAt(i) - '0';
+            sums[i+1] = sums[i] + d;
+            pows[i+1] = (pows[i] * (d != 0 ? 10 : 1)) % mod + d;
+            counts[i+1] = counts[i] + (d != 0 ? 1 : 0);
+            pow10[i+1] = pow10[i] * 10 % mod;
         }
-
-        int[] result = new int[m];
-
+        var ans = new int[m];
         for (int i = 0; i < m; i++) {
-            int l = queries[i][0];
-            int r = queries[i][1];
-
-            int len = prefCnt[r + 1] - prefCnt[l];
-
-            long start = prefVal[l];
-            long end = prefVal[r + 1];
-
-            long x = (end - (start * power[len]) % MOD + MOD) % MOD;
-            long sum = prefSum[r + 1] - prefSum[l];
-
-            result[i] = (int) ((x * sum) % MOD);
+            int l = queries[i][0], r = queries[i][1];
+            long sum = sums[r+1] - sums[l], d = pow10[counts[r+1] - counts[l]];
+            long lv = sum * pows[r+1] % mod, rv = (sum * pows[l] % mod) * d % mod;
+            ans[i] = (int) ((lv - rv + mod) % mod);
         }
-
-        return result;
+        return ans;
     }
 }
