11107248459bf9d47946343bc51e71eeb23050ed
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-07-16 09:56:10 +0530

    Time: 54 ms (90%), Space: 108.1 MB (63.5%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -1,28 +1,27 @@
 class Solution {
-    public long gcdSum(int[] nums) {
-        int[] prefixGcd = new int[nums.length];
-        int max = -1;
-
-        for (int i = 0; i < nums.length; i++) {
-            max = Math.max(max, nums[i]);
-            prefixGcd[i] = gcd(max, nums[i]);
+   
+    public long gcdSum(int[] arr) {
+        int[] prefi = new int[arr.length];
+        int mx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > mx) {
+                mx = arr[i];
+            }
+            prefi[i] = gcd(mx, arr[i]);
         }
-
-        Arrays.sort(prefixGcd);
-
+        Arrays.sort(prefi);
+        int i = 0;
+        int j = arr.length - 1;
         long sum = 0;
-        int i = 0, j = nums.length - 1;
-
         while (i < j) {
-            sum += gcd(prefixGcd[i], prefixGcd[j]);
+            sum += gcd(prefi[i], prefi[j]);
             i++;
             j--;
         }
-
         return sum;
     }
 
-    private int gcd(int a, int b) {
+     public int gcd(int a, int b) {
         while (b != 0) {
             int temp = b;
             b = a % b;
