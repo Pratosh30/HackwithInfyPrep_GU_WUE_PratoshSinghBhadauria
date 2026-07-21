e0a3aa83f272eb6b3aa6f82b733d8c76005c33e6
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-07-21 11:32:21 +0530

    Time: 49 ms (100%), Space: 47.9 MB (83.1%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,42 +1,26 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        char[] arr = s.toCharArray();
-        int n = arr.length;
+        int oneCount = 0; // Total number of ones already present in the string
+        int maxMergedzeros = 0; // Max number ofg aditional ones we could get by merging 2 0 blocks
+        int currZeroCount = 0; // Length of the zero block we are currently walking through
+        int lastZeroCount = 0; // Length of the previous 0 block we finished
 
-        int count1 = 0;
-
-        int block1 = 0;
-        int block2 = 0;
-        int maxx = 0;
-
-        int i = 0;
-        while(i<n){
-            char ch = arr[i];
-
-            if(ch=='0') {
-                block1++;
-                i++;
+        // Loop over each char
+        for(char c : s.toCharArray()){
+            if(c == '0') currZeroCount++; // Increase the continuos zero count
+            else {
+                // If the char is not a 0 and we have a current count of zeros then we save the previous zero block length
+                if(currZeroCount != 0) lastZeroCount = currZeroCount;
+                currZeroCount = 0; // Reset the current zero block length to zero since we are counting the continuos ones in this point
+                oneCount++; // Increase the one count
             }
-
-            else{
-                while(i<n && arr[i]=='1'){
-                    i++;
-                    count1++;
-                }
-                
-                while(i<n && arr[i]=='0'){
-                    i++;
-                    block2++;
-                }
-
-                if(block1!=0 && block2!=0) maxx = Math.max(maxx, block1+block2);
-                block1 = block2;
-                block2 = 0;
-            }
-
+            // Updates the largest merged zero blocks
+            maxMergedzeros = Math.max(maxMergedzeros, currZeroCount + lastZeroCount);
         }
-
-        return count1 + maxx;
-
+        // If there are no 1 blocks then the best zero block comes from either side, so we can't merge 0 blocks 
+        // Meaning there is  no profitable trade making the existing ones the best already
+        if(maxMergedzeros == currZeroCount || maxMergedzeros == lastZeroCount) return oneCount;
+        // Return the largets gain obtainable by merging two zero blocks with the ones already present
+        return oneCount + maxMergedzeros;
     }
 }
