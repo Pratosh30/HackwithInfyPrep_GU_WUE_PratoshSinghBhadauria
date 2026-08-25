ed7999e2caf81d2cdc01ccfbe1b3456d91c7ecee
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-08-25 19:28:00 +0530

    Time: 0 ms (100%), Space: 44.3 MB (99.61%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,13 +1,17 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        long[] x = {0L, 0L};
-        for (int n : nums)
-            if (n % k == 0) {
-                int i = n / k - 1;
-                x[i >> 6] |= 1L << (i & 63);
+        for(int multiple=k;;multiple+=k){
+            boolean found=false;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==multiple){
+                    found=true;
+                    break;
+                }
             }
-            
-        int z = x[0] == -1L ? 1 : 0;
-        return (z * 64 + Long.numberOfTrailingZeros(++x[z] & -x[z]) + 1) * k;
+            if(!found){
+            return multiple;
+            }
+        }
+        
     }
 }
