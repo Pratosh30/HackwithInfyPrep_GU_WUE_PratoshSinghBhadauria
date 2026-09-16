4f5e07e1ca5cca7e9f767c85ea7987f24eaad854
Pratosh singh Bhadauria <132385178+Pratosh30@users.noreply.github.com>
2026-09-16 12:18:19 +0530

    Time: 17 ms (82.58%), Space: 42.3 MB (84.52%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,39 +1,26 @@
 class Solution {
-    static final int MOD = 1000000007;
-    static final int LIMIT = 1999;
-    static long[] fact = new long[LIMIT];
-    static long[] inv = new long[LIMIT];
-
-    static {
-        fact[0] = 1;
-
-        for (int i = 1; i < LIMIT; i++)
-            fact[i] = fact[i - 1] * i % MOD;
-
-        inv[LIMIT - 1] = pow(fact[LIMIT - 1], MOD - 2);
-
-        for (int i = LIMIT - 1; i > 0; i--)
-            inv[i - 1] = inv[i] * i % MOD;
-    }
-
-    private static long pow(long x, int n) {
-        long ans = 1;
-
-        for (; n > 0; n /= 2) {
-            if (n % 2 > 0)
-                ans = ans * x % MOD;
-
-            x = x * x % MOD;
-        }
-
-        return ans;
-    }
-
-    private long comb(int n, int m) {
-        return fact[n] * inv[m] % MOD * inv[n - m] % MOD;
-    }
+    private static final int mod = (int)1e9 + 7;
 
     public int numberOfSets(int n, int k) {
-        return (int) comb(n + k - 1, k * 2);
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) dp[i] = 1;
+
+        int sum = 0;
+        for (int i = 1; i <= k; i++){
+            sum = dp[0];
+            dp[0] = 0;
+            for (int j = 1; j < n; j++){
+                int total = sum;
+                sum = (sum + dp[j]) % mod;
+                dp[j] = (dp[j-1] + total) % mod;
+            }
+        }
+
+        return dp[n-1];
     }
 }
+
+/*
+
+*/
